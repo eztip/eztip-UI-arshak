27702a5455fd807f078ee96d9723e8a8d86718e0
AAsriyan <aasriyan@protonmail.com>
2019-02-07 10:02:02 -0500

    Added GreenSock animation to my nav bar object on the click event listener for my menu button.

--- JS/navBar.js
+++ JS/navBar.js
@@ -7,7 +7,12 @@ class NavLink {
   }
 
   toggleContent() {
-    this.nav.classList.toggle('toggleOn'); 
+    this.nav.classList.toggle('toggleOn');
+    if (this.nav.classList.contains("toggleOn")) {
+      TweenMax.fromTo(".top-nav", 0.5, {right: -550}, {right: 0});
+    } else if (!(this.nav.classList.contains("toggleOn"))) {
+      TweenMax.fromTo(".top-nav", 0.5, {right: 0}, {right: -550});
+    }
   }
 
   resize() {
@@ -18,3 +23,9 @@ class NavLink {
 
 const nav = document.getElementsByClassName('nav-bar');
 Array.from(nav).forEach( nav => new NavLink(nav));
+
+//TweenMax.to(".top-nav", 1.5, {right: 0, opacity:1, scale:1});
+
+
+//TweenMax.fromTo(".top-nav", 1.5, {right: -550}, {right: 0});
+//TweenMax.from(".top-nav", 1.5, {right: 100, opacity:0, scale:0.5});
